bc94ad4faff4618f85fded021b56647da31dd06b
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-09-13 10:00:39 -0700

    fix load test

--- locustfile.py
+++ locustfile.py
@@ -3,16 +3,27 @@ import uuid
 from locust import HttpUser, task, between
 
 class MyUser(HttpUser):
-    wait_time = between(0.5, 1)  # Random wait time between requests
+    wait_time = between(1, 3)  # Random wait time between requests
 
-    @task(100)
+    @task
     def litellm_completion(self):
         # no cache hits with this
+        # Customize the payload with "model" and "messages" keys
         payload = {
-            "model": "gemini-vision",
-            "messages": [{"role": "user", "content": f"{uuid.uuid4()} This is a test there will be no cache hits and we'll fill up the context" * 150 }],
-            "user": "my-new-end-user-1"
+            "model": "fake-azure-endpoint",
+            "messages": [
+                {
+                    "role": "user", 
+                    "content": [
+                                {"type": "text", "text": "What is in this image?"},
+                                {"type": "image_url",
+                                "image_url": {"url": "https://i.etsystatic.com/6267543/r/il/077fe2/3271243073/il_1588xN.3271243073_cmph.jpg"}
+                                }
+                    ]
+                }
+            ]
         }
+
         response = self.client.post("chat/completions", json=payload)
         if response.status_code != 200:
             # log the errors in error.txt
