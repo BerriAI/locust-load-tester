887f8356831e4fa52216d19fbbafac9f52d133fb
Krish Dholakia <krrishdholakia@gmail.com>
2024-09-14 19:56:48 -0700

    change model to bedrock

--- locustfile.py
+++ locustfile.py
@@ -10,7 +10,7 @@ class MyUser(HttpUser):
         # no cache hits with this
         # Customize the payload with "model" and "messages" keys
         payload = {
-            "model": "gemini-vision",
+            "model": "fake-claude-endpoint",
             "messages": [
                 {
                     "role": "user", 
