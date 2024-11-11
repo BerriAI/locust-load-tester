429c98458b3ede484043728a1bf9ac41acc92b00
Ishaan Jaff <ishaanjaffer0324@gmail.com>
2024-11-11 15:31:58 -0800

    use gemini vision

--- locustfile.py
+++ locustfile.py
@@ -10,7 +10,7 @@ class MyUser(HttpUser):
         # no cache hits with this
         # Customize the payload with "model" and "messages" keys
         payload = {
-            "model": "fake-openai-endpoint",
+            "model": "gemini-vision",
             "messages": [
                 {
                     "role": "user", 
